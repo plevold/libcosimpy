0287283a0977750a3a77562197f60e2aaef47aa9
Pål Levold <paal@levold.net>
2025-01-07 14:47:04 +0100

    TEMPORARY: Commit statically linked libcosimc

--- hatch_build.py
+++ hatch_build.py
@@ -12,19 +12,19 @@ class WheelHook(BuildHookInterface):
         super().initialize(version, build_data)
         build_data["infer_tag"] = True
         build_data["pure_python"] = False
-        system_os = platform.system()
-        conan, _, _ = Conan.factory()
-        conan.config_set("general.revisions_enabled", "True")
-        conan.remote_add(
-            remote_name="osp",
-            url="https://osp.jfrog.io/artifactory/api/conan/conan-local",
-            force=True,
-            insert=0,
-        )
-        conan.install(
-            path="conan",
-            install_folder="build",
-            lockfile='conan/conan-linux64.lock' if system_os == "Linux" else 'conan/conan-win64.lock',
-        )
-        if system_os == "Linux":
-            os.system("patchelf --set-rpath '$ORIGIN' build/libcosimc/*")
+        # system_os = platform.system()
+        # conan, _, _ = Conan.factory()
+        # conan.config_set("general.revisions_enabled", "True")
+        # conan.remote_add(
+        #     remote_name="osp",
+        #     url="https://osp.jfrog.io/artifactory/api/conan/conan-local",
+        #     force=True,
+        #     insert=0,
+        # )
+        # conan.install(
+        #     path="conan",
+        #     install_folder="build",
+        #     lockfile='conan/conan-linux64.lock' if system_os == "Linux" else 'conan/conan-win64.lock',
+        # )
+        # if system_os == "Linux":
+        #     os.system("patchelf --set-rpath '$ORIGIN' build/libcosimc/*")
